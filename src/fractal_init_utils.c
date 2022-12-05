1f0d30ffb429f1c1e5cf5a6a10fffc6b52a6bc0d
Stefan Dukic <sdukic@3-F-14.42heilbronn.de>
2022-12-05 14:07:45 +0100

    fix: implement resizing for other fractals than mandelbrot

--- src/fractal_init_utils.c
+++ src/fractal_init_utils.c
@@ -6,7 +6,7 @@
 /*   By: sdukic <sdukic@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 19:29:30 by sdukic            #+#    #+#             */
-/*   Updated: 2022/12/05 12:43:05 by sdukic           ###   ########.fr       */
+/*   Updated: 2022/12/05 13:55:55 by sdukic           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,13 +28,13 @@ void	ft_initialize_fractal(t_fractal *fractal, char *name,
 	t_complex c, t_vector screen_dim)
 {
 	if (!ft_strcmp(name, "julia"))
-		ft_initialize_julia(fractal, c);
+		ft_initialize_julia(fractal, c, screen_dim);
 	else if (!ft_strcmp(name, "eye"))
-		ft_initialize_eye(fractal);
+		ft_initialize_eye(fractal, screen_dim);
 	else if (!ft_strcmp(name, "mandelbrot3"))
-		ft_initialize_mandelbrot3(fractal);
+		ft_initialize_mandelbrot3(fractal, screen_dim);
 	else if (!ft_strcmp(name, "mandelbrot4"))
-		ft_initialize_mandelbrot4(fractal);
+		ft_initialize_mandelbrot4(fractal, screen_dim);
 	else
 		ft_initialize_mandelbrot(fractal, screen_dim);
 }
